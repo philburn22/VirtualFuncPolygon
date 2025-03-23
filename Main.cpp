3d695eefa2b02a3af0a16266018aba5111b1f35b
philburn22 <143739633+philburn22@users.noreply.github.com>
2025-03-23 15:21:56 -0400

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,22 @@
+/*************************************************************************
+** Author                : Nathan Philburn
+** Program               : hw1, q2
+** Date Created          : March 20, 2025
+** Date Last Modified    : March 23, 2025
+** Usage                 : No command line arguments
+**
+** Problem:
+** Modify the Polygon class to include two pure virtual functions:
+** - getArea() which returns an int.
+** - draw() which returns void.
+** Create two derived classes: Rectangle and Triangle.
+** Override getArea() to calculate the area (width * height for Rectangle,
+** (width * height)/2 for Triangle) and override draw() to display the shape
+** using stars. Update main() to demonstrate the functionality of these classes.
+*************************************************************************/
+#include <iostream>
+#include "Polygon.h"
+using namespace std;
+int main() {
+    return 0;
+}
